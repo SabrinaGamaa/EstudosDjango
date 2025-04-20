0453103c482f073317bdc9192dab59c9d7caa770
Sabrina Gama <62630517+SabrinaGamaa@users.noreply.github.com>
2025-04-19 22:15:11 -0300

    Criando Profile

--- a_core/urls.py
+++ a_core/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from a_posts.views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from a_users.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,6 +31,8 @@ urlpatterns = [
     path('post/delete/<pk>/', post_delete_view, name='delete-post'),
     path('post/edit/<pk>/', post_edit_view, name='post-edit'),
     path('post/<pk>/', post_page_view, name='post'),
+    path('profile/', profile_view, name='profile'),
+    path('profile/edit/', profile_edit_view, name='profile-edit')
     
 ]
 
